94efd52bc5976fa876a0f96ccbd62d220b60989c
Alexis Davalos <alexisdavalos@Alexiss-MacBook-Pro.local>
2021-08-02 20:49:38 -0500

    removed console log

--- pages/about.js
+++ pages/about.js
@@ -5,36 +5,35 @@ import { getPostBySlug } from "../lib/staticApi";
 const Layout = dynamic(import("../components/layout"));
 
 export default function About({ post }) {
-	console.log({ post });
-	return (
-		<>
-			<Layout>
-				<PostBody content={post.content} />
-			</Layout>
-		</>
-	);
+    return (
+        <>
+            <Layout>
+                <PostBody content={post.content} />
+            </Layout>
+        </>
+    );
 }
 
 export async function getStaticProps() {
-	const fields = [
-		"title",
-		"date",
-		"slug",
-		"author",
-		"content",
-		"topics",
-		"coverImage",
-		"excerpt",
-	];
+    const fields = [
+        "title",
+        "date",
+        "slug",
+        "author",
+        "content",
+        "topics",
+        "coverImage",
+        "excerpt",
+    ];
 
-	// slug and fields to fetch
-	const post = getPostBySlug("about", fields);
+    // slug and fields to fetch
+    const post = getPostBySlug("about", fields);
 
-	// By returning { props: posts }, the Blog component
-	// will receive `posts` as a prop at build time
-	return {
-		props: {
-			post,
-		},
-	};
+    // By returning { props: posts }, the Blog component
+    // will receive `posts` as a prop at build time
+    return {
+        props: {
+            post,
+        },
+    };
 }
